ef94c1b49cc9f988bb36ba7cf19abe4a4f890740
vikash bharti <vikasharyan7739@gmail.com>
2025-09-18 23:20:14 +0530

    first commit of my portfolio

---
+++ animation.js
@@ -0,0 +1,11 @@
+new Typed("#skills",{
+
+    strings:['Java FullStack Developer','Web Developer','Front-End Developer','Back-End Developer'],
+    typeSpeed:50,
+    backSpeed:50,
+    loop:true,
+    startDelay:500,
+    backDelay:500,
+    showCursor:true
+}
+)
